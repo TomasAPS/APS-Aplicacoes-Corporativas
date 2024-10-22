228a2280eeb20540af89d9bad034e1d0c6105f22
TomasAPS <castroc.tomas@gmail.com>
2024-10-22 16:29:45 -0300

    Create SecurityConfig.java

---
+++ SecurityConfig.java
@@ -0,0 +1,22 @@
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/api/users/register").permitAll()
+            .antMatchers("/api/cars/**").authenticated()
+            .and()
+            .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password("{noop}adminpass")
+            .roles("ADMIN");
+    }
+}
