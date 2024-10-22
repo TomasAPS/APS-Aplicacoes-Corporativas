0c16be38a80be0bb835f71f46759e7c56d8792e5
TomasAPS <castroc.tomas@gmail.com>
2024-10-22 16:29:21 -0300

    Create CarController.java

---
+++ CarController.java
@@ -0,0 +1,24 @@
+@RestController
+@RequestMapping("/api/cars")
+public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+    @GetMapping
+    public List<Car> listCars() {
+        return carService.listAllCars();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addCar(@RequestBody Car car) {
+        carService.addCar(car);
+        return ResponseEntity.ok("Car added successfully!");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCar(@PathVariable Long id) {
+        carService.deleteCar(id);
+        return ResponseEntity.ok("Car deleted successfully!");
+    }
+}
